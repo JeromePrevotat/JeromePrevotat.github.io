d3e6fdcf49da52861b1106a176333a9b16af5761
unknown <jer.prevotat@gmail.com>
2025-02-10 11:51:29 +0100

    2nd pass js + some bugfix/rework

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Trello : https://trello.com/b/oW2awB1Z/outils-collaboratifs
 Github : https://github.com/JeromePrevotat/Outils-Collaboratifs
 
 Bugfix:
-- Add . at the end of project description
-- Remove project placeholders
+- Add . at the end of project description. DONE
+- Remove project placeholders. REWORKED
--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,5 @@
-displayed = 0;
+displayed = 0
+arr_projects =[]
 
 class Project{
     constructor(name, desc, url){
@@ -8,10 +9,11 @@ class Project{
     }
 }
 
-const p1 = new Project("Bot Discord", "Bot Discord pour un jeu mobile retournant les caractéristiques et capacités des différents personnages en réponse aux commandes des utilisateurs","https://github.com/JeromePrevotat/SWDiscordBot");
-const p2 = new Project("Portfolio","Portfolio fait avec Python et Django présentant mes projets persos stockés dans une DB SQL","https://github.com/JeromePrevotat/Portfolio");
-const p3 = new Project("Messagerie Instantannée","Messagerie Instantannée en ligne dont les parties client et serveur sont faites en Python","https://github.com/JeromePrevotat/ProjectM");
-arr_projects = [p1,p2,p3];
+const placeholder = new Project("This is a Placeholder", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima natus voluptatum incidunt cumque. Ullam velit natus iste asperiores ex culpa non! Laudantium officiis dicta quasi deserunt, consequuntur cumque enim nobis!","Points to nowhere");
+const p1 = new Project("Bot Discord", "Bot Discord pour un jeu mobile retournant les caractéristiques et capacités des différents personnages récupérées via une API tierce en réponse aux différentes commandes des utilisateurs, le tout sous forme d'un embed pour une mise en forme claire et concise.","https://github.com/JeromePrevotat/SWDiscordBot");
+const p2 = new Project("Portfolio","Portfolio fait avec Python et Django présentant mes projets personnels stockés dans une DB SQL.","https://github.com/JeromePrevotat/Portfolio");
+const p3 = new Project("Messagerie Instantannée","Messagerie Instantannée en ligne dont les parties client et serveur sont faites en Python. Pas de voice, uniquement du texte. UI faite avec le module tkinter. Cela m'a introduit aux problèmes de queueing et programmation asynchrone.","https://github.com/JeromePrevotat/ProjectM");
+arr_projects = [placeholder,p1,p2,p3];
 
 function update_project_info(displayed){
     document.getElementById("project-name").innerHTML = arr_projects[displayed].name;
